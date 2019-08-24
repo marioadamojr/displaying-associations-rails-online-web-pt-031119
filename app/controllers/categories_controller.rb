adbb35feb4f09aadfd06eb26aa3718ad35d4bb05
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-24 22:56:03 +0000

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,8 +12,8 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    category = Category.create(params[:category])
-    redirect_to category_path(category)
+    category = Category.create(category_params(:name))
+    redirect_to category_path(@category)
   end
 
   def edit
@@ -21,8 +21,14 @@ class CategoriesController < ApplicationController
   end
 
   def update
-    category = Category.find(params[:id])
-    category.update(params.require(:category))
-    redirect_to category_path(category)
+    @category = Category.find(params[:id])
+    @category.update(category_params(:name))
+    redirect_to category_path(@category)
+  end
+
+  private
+
+  def category_params(*args)
+    params.require(:category).permit(*args)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,8 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		post = Post.create(params[:post])
-		redirect_to post_path(post)
+		post = Post.create(post_params(:title, :description, :post_status, :author_id, :category_id))
+		redirect_to post_path(@post)
 	end
 
 	def edit
@@ -23,8 +23,14 @@ class PostsController < ApplicationController
 	end
 
 	def update
-		post = Post.find(params[:id])
-		post.update(params.require(:post))
-		redirect_to post_path(post)
+		@post = Post.find(params[:id])
+		@post.update(post_params(:title, :description, :post_status, :author_id, :category_id))
+		redirect_to post_path(@post)
+	end
+
+	private
+
+	def post_params(*args)
+		params.require(:post).permit(*args)
 	end
 end
