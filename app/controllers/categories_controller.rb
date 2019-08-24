f29b3cbd28822dae03eb553bb033f91fd0554375
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-24 22:53:17 +0000

    Automatically backed up by Learn

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,7 +12,7 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    category = Category.create(params[:category])
+    category = Category.create(category_params(:name))
     redirect_to category_path(@category)
   end
 
